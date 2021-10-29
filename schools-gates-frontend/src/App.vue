30a19d079ce6109eebff123acc5af58347c1f7a5
amer <amer23zx@gmail.com>
2021-10-29 16:05:39 +0800

    first commit

---
+++ schools-gates-frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Start up</div>
+</template>
+
+<script>
+import Test from "./graphql/test.gql";
+export default {
+  name: "App",
+  data: () => ({
+    //
+  }),
+  apollo: {
+    testingData: {
+      query: Test,
+      update(data) {
+        console.log(data);
+        return data;
+      },
+    },
+  },
+};
+</script>
---
+++ schools-gates-frontend/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+
+  pluginOptions: {
+    apollo: {
+      enableMocks: false,
+      enableEngine: false
+    }
+  }
+}
